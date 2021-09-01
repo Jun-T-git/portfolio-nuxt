ebcd91a8fa19199d3de9a3806de10f803ef99064
Jun-T-git <jun0303t@icloud.com>
2021-09-01 19:46:45 +0900

    設定変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,27 @@
+require('dotenv').config()
+const { QIITA_API_ACCESS_TOKEN } = process.env
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'portfolio-nuxt',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ['@/plugins/composition-api'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -33,13 +32,20 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/composition-api/module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: ['@nuxtjs/axios'],
+  axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
+  generate: {
+    interval: 2000,
+  },
+
+  env: {
+    QIITA_API_ACCESS_TOKEN,
+  },
 }
